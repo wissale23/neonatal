c63cc58e010e791a2f9b4f45e05a4e982d351915
Rebecca Shrestha <rs3123@ic.ac.uk>
2025-12-17 17:42:19 +0000

    Update NurseServlet.java

--- src/main/java/NurseServlet.java
+++ src/main/java/NurseServlet.java
@@ -1,2 +1,9 @@
+import javax.servlet.http.*;
+import java.io.IOException;
+
 public class NurseServlet {
+  public void display(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        GlucoseGraph graph = new GlucoseGraph(data);
+        resp.getWriter().write(graph.generateHTML(true, false)); // nurse input
+    }
 }
