530e3cd39a2dcac09b2f0a0494f7d8f0f3ab0593
tobymeek <tobymeek24@gmail.com>
2025-12-22 09:17:14 +0000

    Debugging researcher download

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -194,7 +194,7 @@ public class Servlet extends HttpServlet {
                     "<h1>Researcher Portal</h1>" +
                             "<p>Download glucose monitoring data:</p>" +
                             "<form method=\"POST\" action=\"" + req.getContextPath() + "/researchers\">" +
-                            "<button type=\"button\" name=\"action\" value=\"download\">Download Data</button>" +
+                            "<button type=\"submit\" name=\"action\" value=\"download\">Download Data</button>" +
                             "</form>" +
                             "<p><a href=\"" + req.getContextPath() + "/logout\">Logout</a></p>"
             );
