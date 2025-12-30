d7047404c4b0e04d20b009cf8edbef1818cfe5b9
Wissale Zegzaoui <wz2923@ic.ac.uk>
2025-12-30 02:22:44 +0000

    Update Nurse.java

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,5 +1,13 @@
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 
 public class Nurse extends Adult implements Pageable {
     
