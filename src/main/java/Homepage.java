eaf5787eb6ed1b2672482246522f6af7f819398d
rebeccashrestha <rs3123@ic.ac.uk>
2026-01-11 19:45:16 +0000

    Test context path

--- src/main/java/Homepage.java
+++ src/main/java/Homepage.java
@@ -52,7 +52,7 @@ public class Homepage {
                 + "    <img src='" + contextPath + "/images/2.jpg' style='width:100%'>"
                 + "  </div>"
                 + "  <div class='mySlides fade'>"
-                + "    <img src='" + contextPath + "images/3.jpg' style='width:100%'>"
+                + "    <img src='" + contextPath + "/images/3.jpg' style='width:100%'>"
                 + "  </div>"
                 + "  <a class='prev' onclick='plusSlides(-1)'>&#10094;</a>"
                 + "  <a class='next' onclick='plusSlides(1)'>&#10095;</a>"
