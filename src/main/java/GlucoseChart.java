1bf5637fd3d244aa8fa0e5d785c3f1824b2aafdb
Wissale Zegzaoui <wz2923@ic.ac.uk>
2026-01-11 18:55:43 +0000

    Implement logout button in GlucoseChart
    Add a logout button with custom styling in GlucoseChart.java

--- src/main/java/GlucoseChart.java
+++ src/main/java/GlucoseChart.java
@@ -126,6 +126,20 @@ public class GlucoseChart {
         }
         return feedingBars;
     }
+    
+
+    public String logoutButton(HttpServletRequest req) {
+        return "<a href='" + req.getContextPath() + "/' "
+                + "style='position:absolute; top:10px; left:10px;"
+                + "background-color:#ffc0cb;"  
+                + "border:2px solid black;"
+                + "padding:5px 10px; border-radius:4px;"
+                + "color:black; font-weight:bold; text-decoration:none; cursor:pointer;"
+                + "transition: background-color 0.2s;'>"
+                + "Home</a>";
+    }
+    
+
 
     // Display warning alert message box
     public String buildWarningHTML(List<Double> glucoseData) {
