229470cc4e5e1b26691776a36a6f2d632edb93bd
rebeccashrestha <rs3123@ic.ac.uk>
2026-01-11 19:34:05 +0000

    Test context path

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -74,8 +74,9 @@ public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
         throws ServletException, IOException {
-        
+
         String path = req.getServletPath();
+
 
         if ("/".equals(path)){
             resp.setContentType("text/html");
