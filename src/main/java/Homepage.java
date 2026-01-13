c2c16e0ca5ccf399c2719aa0192ca5bdab05d8bf
rebeccashrestha <rs3123@ic.ac.uk>
2026-01-13 16:42:48 +0000

    Commit ParentChart.java

--- src/main/java/Homepage.java
+++ src/main/java/Homepage.java
@@ -73,7 +73,7 @@ public class Homepage {
         return ""
                 + "<style>"
                 + "*{box-sizing:border-box;}"
-                + ".slideshow-container{max-width:1750px;position:relative;margin:40px auto;}"
+                + ".slideshow-container{max-width:1000px;position:relative;margin:40px auto;}"
                 + ".mySlides{display:none;}"
                 + ".mySlides img{width:100%;height:400px;object-fit:cover;}"
 
