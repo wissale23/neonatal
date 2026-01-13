e6e7d81796d17bdc79a3c98d00a3c6527e57fb62
rebeccashrestha <rs3123@ic.ac.uk>
2026-01-13 13:10:01 +0000

    Header to Homepage

--- src/main/java/Homepage.java
+++ src/main/java/Homepage.java
@@ -10,6 +10,8 @@ public class Homepage {
                 + "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">"
                 + "<style>"
                 + "body { font-family: 'Lato', sans-serif; }"
+
+                // Sidebar CSS
                 + ".sidebar { height:100%; width:0; position:fixed; z-index:1; top:0; left:0; background-color:#111; overflow-x:hidden; transition:0.5s; padding-top:60px; }"
                 + ".sidebar a { padding:8px 8px 8px 32px; text-decoration:none; font-size:25px; color:#818181; display:block; transition:0.3s; }"
                 + ".sidebar a:hover { color:#f1f1f1; }"
@@ -17,10 +19,15 @@ public class Homepage {
                 + ".openbtn { font-size:20px; cursor:pointer; background-color:#111; color:white; padding:10px 15px; border:none; }"
                 + ".openbtn:hover { background-color:#444; }"
                 + "#main { transition: margin-left .5s; padding:16px; }"
-                + "@media screen and (max-height:450px){.sidebar{padding-top:15px;}.sidebar a{font-size:18px;}}"
+                + "@media screen and (max-height:450px){.sidebar{padding-top:15px;}.sidebar a{font-size:18px;}}" + "</style>"
+
+                // Header CSS
+                + ".header { background-color: navy; color: white; text-align: center; padding: 20px; font-size: 28px; font-weight: bold; }"
                 + "</style>"
-                + sectionsCSS           // contacts CSS
+                + sectionsCSS  // Contact CSS
+
                 + "</head><body>"
+                + "<div class='header'>Homepage</div>" // Header HTML
                 + generateSidebar()           // sidebar HTML
                 + generateSlideshow(contextPath)         // slideshow content
                 + generateAboutUs()         // about us HTML
