ded242ef2a4c14e51c461fcb0b982b4745ac57cf
rebeccashrestha <rs3123@ic.ac.uk>
2026-01-14 04:00:05 +0000

    Merge Final-Edits into Main

--- src/main/java/Adult.java
+++ src/main/java/Adult.java
@@ -1,21 +1,10 @@
-import java.util.ArrayList;
-
 public abstract class Adult extends Person implements Pageable{
-    private ArrayList<Baby> patients = new ArrayList<>();
     private final String endpoint;
 
     public Adult(String name, int id, String endpoint) {
         super(name, id);
         this.endpoint = endpoint;
 
-    }
-
-    public ArrayList<Baby> getPatients() {
-        return patients;
-    }
-
-    public void addPatient(Baby baby){
-        patients.add(baby);
     }
 
     public String getEndpoint() {
